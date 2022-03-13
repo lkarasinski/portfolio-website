5588ccb97718e4f45dc04a2076244ac3cabd66a5
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-03-13 17:09:13 +0100

    feat: ✨ Add breakpoints object

---
+++ src/utils/constants.ts
@@ -0,0 +1,8 @@
+export const breakpoints = {
+    // Max value for mobile screen width
+    mobile: '540px',
+    // Max value for tablet screen width
+    tablet: '1024px',
+    // Min value for desktop screen width
+    desktop: '1025px',
+};
