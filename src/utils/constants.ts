e4e348bfdd0e585b09f8a2e2ab3f0a827ccfe5ba
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-03-18 16:36:28 +0100

    feat: ✨ Add colors object

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,3 +6,22 @@ export const breakpoints = {
     // Min value for desktop screen width
     desktop: '1025px',
 };
+
+export const colors = {
+    goldfish: {
+        background: '#F1F5FD',
+        foreground: '#0C235A',
+    },
+    adpero: {
+        background: '#F0F2FF',
+        foreground: '#001166',
+    },
+    daga: {
+        background: '#FFFAF0',
+        foreground: '#664400',
+    },
+    bienbot: {
+        background: '#F1F1FE',
+        foreground: '#0A0A5C',
+    },
+};
