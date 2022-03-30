5ebf89fda73aa97f65c4aa3aea6b3c67d8d9b8f9
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-03-30 16:32:55 +0200

    fix: ✏️ Fix typo

--- src/components/MyWorkPanel/myWorkPanel.tsx
+++ src/components/MyWorkPanel/myWorkPanel.tsx
@@ -37,7 +37,7 @@ const MyWorkPanel = () => {
                         href="/goldfish"
                     />
                     <WorkCard
-                        heading="Biebot"
+                        heading="Bienbot"
                         description="Discord bot with dashboard"
                         imageUrl="/placeholder.png"
                         colorScheme={colors.bienbot}
