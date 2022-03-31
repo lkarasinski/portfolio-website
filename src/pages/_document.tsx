f9162dc69ea31b1fd7aad1b889baaac7f9379f3a
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-03-31 15:12:46 +0200

    feat: 💄 Add light font weight

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,7 @@ class MyDocument extends Document {
                     />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link
-                        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap"
+                        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap"
                         rel="stylesheet"
                     />
                 </Head>
