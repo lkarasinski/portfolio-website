98dc4f6fb76baf7964e31056e303eb840e4133f5
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-04-15 19:18:51 +0200

    chore: :wrench: Change next runtime to nodejs
    Change runtime to nodejs, to allow react-player to work properly

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
         // Enables the styled-components SWC transform
         styledComponents: true,
     },
+    experimental: {
+        runtime: 'nodejs',
+    },
 };
 
 module.exports = nextConfig;
