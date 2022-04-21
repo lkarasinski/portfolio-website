ffe76344d79c74190136d69818708c46de3b9d57
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-04-21 14:51:49 +0200

    fix: 💄  Add responsive font size and remove padding

--- src/components/TextSection/textSection.style.tsx
+++ src/components/TextSection/textSection.style.tsx
@@ -4,5 +4,16 @@ import styled from 'styled-components';
 export const StyledWrapper = styled.section`
     max-width: 1024px;
     margin: 0 auto;
-    padding: 10px;
+
+    p {
+        @media (min-width: ${breakpoints.desktop}) {
+            font-size: 20px;
+        }
+        @media (max-width: ${breakpoints.tablet}) {
+            font-size: 18px;
+        }
+        @media (max-width: ${breakpoints.mobile}) {
+            font-size: 16px;
+        }
+    }
 `;
