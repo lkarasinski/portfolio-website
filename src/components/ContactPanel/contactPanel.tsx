6fa76d90c3d0b0ff53938e8f339aeb38ecdeb57f
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-04-23 19:33:06 +0200

    feat: :sparkles: Improve links in contact panel

--- src/components/ContactPanel/contactPanel.tsx
+++ src/components/ContactPanel/contactPanel.tsx
@@ -3,20 +3,25 @@ import Link from 'next/link';
 import { StyledWrapper } from './contactPanel.style';
 
 const ContactPanel = () => {
+    const [url, setUrl] = React.useState('');
+    React.useEffect(() => {
+        setUrl(location.origin.replace(/^https?:\/\//, ''));
+    }, []);
+
     return (
         <StyledWrapper>
             <h2>Contact</h2>
             <p>
-                email:{' '}
+                email:&#3;
                 <a href="mailto:lukasz.karasinski.15@gmail.com">
                     lukasz.karasinski.15@gmail.com
                 </a>
             </p>
             <p>
-                github: <Link href="/github">lkarasinski.pl/github</Link>
+                github: <Link href="/github">{`${url}/github`}</Link>
             </p>
             <p>
-                linkedin: <Link href="/linkedin">lkarasinski.pl/linkedin</Link>
+                linkedin: <Link href="/linkedin">{`${url}/linkedin`}</Link>
             </p>
         </StyledWrapper>
     );
