2d803d3b9a9b935986a0e733b8be6c95d0bf54dc
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-04-27 18:24:13 +0200

    fix: :wrench: Change image loader to akamai

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    images: {
+        loader: 'akamai',
+        path: '',
+    },
     compiler: {
         // Enables the styled-components SWC transform
         styledComponents: true,
