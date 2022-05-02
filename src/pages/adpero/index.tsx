fe5b8e2ca59dff2b3016c544dd1ae12c98335966
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-05-02 12:53:04 +0200

    feat: :sparkles: Create Adpero page

---
+++ src/pages/adpero/index.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import type { NextPage } from 'next';
+import { PageTransition, ProjectPage } from '@components';
+import { colors } from '@constants';
+
+const AdperoPage: NextPage = () => {
+    return (
+        <PageTransition>
+            <ProjectPage
+                title="Adpero"
+                video="placeholder.mp4"
+                colorScheme={colors.adpero}
+                technologies={[
+                    'react.svg',
+                    'typescript.svg',
+                    'next.js.svg',
+                    'nx.svg',
+                    'firebase.svg',
+                    'styledComponents.svg',
+                    'formik.svg',
+                    'framerMotion.svg',
+                    'cypress.svg',
+                    'vercel.svg',
+                ]}
+            >
+                <p>
+                    Makes it easy to organize your journeys by displaying all
+                    the important information in one place. Allows you to share
+                    your plans with friends simply by inviting them to your
+                    journey. Gives you ability to create polls to make decisions
+                    together. Converts total cost of your journey to your
+                    currency
+                    <br />
+                    <br />
+                    This app was designed entirely by me, and since it&#39;s
+                    very interactive, there were many UX/UI problems that I had
+                    to solve. Because of that this project was rebuilt a couple
+                    of times, each time using a different tech stack. Eventually
+                    I&#39;ve settled with Next.js as the front-end framework,
+                    and the app was created using Nx Monorepo. While coding this
+                    project I&#39;ve overcame many challenges such as creating
+                    and validating dynamic forms, handling javascript dates, and
+                    adding concurrent support for offline and online data
+                    storage. This is the biggest app I&#39;ve created as for
+                    now, and I plan to add more features in the future.
+                </p>
+            </ProjectPage>
+        </PageTransition>
+    );
+};
+
+export default AdperoPage;
---
+++ src/pages/adpero/live.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const AdperoLivePage: NextPage = () => {
+    return (
+        <Head>
+            <meta
+                httpEquiv="refresh"
+                content="0;url=https://adpero.vercel.app/"
+            />
+        </Head>
+    );
+};
+
+export default AdperoLivePage;
