c8257927f78f48cba35b3cdf9ec076a1a98a2041
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-05-31 16:28:20 +0200

    feat: :chart_with_upwards_trend: Add hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
+import * as React from 'react';
 import { Layout } from '@components';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { hotjar } from 'react-hotjar';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    React.useEffect(() => {
+        hotjar.initialize(2997008, 6);
+    }, []);
+
     return (
         <>
             <Head>
