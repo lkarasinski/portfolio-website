c54a6162148f39bc0649bdf39b8bdc141fa3f590
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-05-31 15:57:20 +0200

    refactor: :lipstick: Add page title and update favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { Layout } from '@components';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <>
+            <Head>
+                <title>Lukasz Karasinski</title>
+            </Head>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
     );
 }
 
