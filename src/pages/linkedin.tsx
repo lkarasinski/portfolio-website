b58c9d2c8a4626a90f0a01f6c296bb5dee7ddc5e
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-05-31 16:13:14 +0200

    feat: :sparkles: Add redirect pages

---
+++ src/pages/linkedin.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const LinkedinPage: NextPage = () => {
+    return (
+        <Head>
+            <meta
+                httpEquiv="refresh"
+                content="0;url=https://www.linkedin.com/in/%C5%82ukasz-karasi%C5%84ski-0a854b195/"
+            />
+        </Head>
+    );
+};
+
+export default LinkedinPage;
