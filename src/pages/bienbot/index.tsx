e04a7be4bab00d17b523b826ab89467b77b7bb70
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2022-06-01 12:16:33 +0200

    feat: :speech_balloon: Add BienBot project description

--- src/pages/bienbot/index.tsx
+++ src/pages/bienbot/index.tsx
@@ -24,7 +24,35 @@ const BienbotPage: NextPage = () => {
                 ]}
                 liveDisabled
             >
-                <p>Work in progress</p>
+                <p>
+                    Work in progress
+                    <br />
+                    <br />
+                    This project consists of a Frontend made in Next.js, a
+                    Backend made in Express and a Discord bot made in
+                    Discord.js.
+                    <br />
+                    <br />
+                    The dashboard was designed by me and provides easy insight
+                    into the statistics of discord servers. The UI Component
+                    Library was created using Storybook, Styled Components and
+                    Framer Motion.
+                    <br />
+                    <br />
+                    So far, only the Dashboard and the User&#39;s page are done,
+                    but work on the design of the other pages is underway. And
+                    since I&#39;m not a UI Designer, it takes the longest time
+                    to create.
+                    <br />
+                    <br />
+                    When all the pages are finished, we will implement state
+                    management using the Redux Toolkit library.
+                    <br />
+                    <br />I am creating this project with{' '}
+                    <a target="blank" href="https://github.com/mattponiat">
+                        @mattponiat
+                    </a>
+                </p>
             </ProjectPage>
         </PageTransition>
     );
